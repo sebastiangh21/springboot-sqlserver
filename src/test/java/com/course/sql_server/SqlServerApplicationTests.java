bdfc87562bf427d660c59754f88ced8b868a28f8
sebastiangh21 <sebastiangh21@gmail.com>
2024-08-10 01:14:14 -0500

    Proyecto inicial de la api con sql server

---
+++ src/test/java/com/course/sql_server/SqlServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.sql_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
